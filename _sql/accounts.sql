95b13430ec50d647c22e857b4b7f81bbc798c704
HJin.me <hjin.me@qq.com>
2022-12-12 10:09:33 +0800

    feat: 稍微调整一下页面字号

--- _sql/accounts.sql
+++ _sql/accounts.sql
@@ -1,6 +1,8 @@
 create table accounts
 (
-    id           text                     not null,
+    id           text                     not null
+        constraint pk_id
+            primary key,
     display_name text                     not null,
     role         text                     not null,
     created_at   timestamp with time zone not null
